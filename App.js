da4c419e6dab7e691065277a67564892d8f1ed43
PriyankaAMehta <prayankagm@gmail.com>
2025-12-17 14:52:55 +0100

    Create multiple childrens - use [] array

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 <div id="parent">
     <div id="child">
         <h1></h1> 
+        <h2><h2>
     </div>
 </div>
 */
@@ -11,14 +12,17 @@ react.createElement takes 3 arguments
 2- {} object with tag attributes like id, class etc. 
 3- childrens means something that goes into the tag like other tags or content.
 */
+
+/* adding one more tag that means adding multiple childerns can be done using [] array i.e
+putting the childerns into []. */
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hello Priyanka")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello Priyanka"),
+    React.createElement("h2", {}, "Hello Shreyansh"),
+  ])
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
