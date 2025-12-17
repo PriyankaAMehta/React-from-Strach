c84fcbe446f33e67cb9b6e45b0fa6ec648c416cf
PriyankaAMehta <prayankagm@gmail.com>
2025-12-17 15:02:08 +0100

    Created Complex  Nested Elements using React

--- App.js
+++ App.js
@@ -4,6 +4,10 @@
         <h1></h1> 
         <h2><h2>
     </div>
+    <div id="child">
+        <h1></h1> 
+        <h2><h2>
+    </div>
 </div>
 */
 /*
@@ -16,14 +20,16 @@ react.createElement takes 3 arguments
 /* adding one more tag that means adding multiple childerns can be done using [] array i.e
 putting the childerns into []. */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "Hello Priyanka"),
     React.createElement("h2", {}, "Hello Shreyansh"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello Raj"),
+    React.createElement("h2", {}, "Hello Anita"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
