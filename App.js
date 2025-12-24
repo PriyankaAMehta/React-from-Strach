08c96f6fd7c6d6ca380056d99045ae6b7d690854
PriyankaAMehta <prayankagm@gmail.com>
2025-12-24 13:54:42 +0100

    Since Functional components are normal JS functions , we can call component inside {} as function call

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = function () {
-  return <h1>This is Title 1 </h1>;
-};
-const elem = <span>and she is awesome!!</span>;
-let number = 1000;
-let a = "Priyanka";
-const subheading = <h1>PRIYANKA</h1>;
+const Title = () => <h1>This is Title 1 </h1>;
 
 const HeadingComponent = () => (
   <div id="container">
-    {number}
-    <h2>{number}</h2>
-    {300 + 30}
-    <h2>This is {a}</h2>
-    <h2>{console.log("ndjn")}</h2>
-
-    <Title />
-    <h1>This is Heading component {elem}</h1>
-    {subheading}
+    {Title()}
+    <h1>This is Heading component</h1>
   </div>
 );
 
