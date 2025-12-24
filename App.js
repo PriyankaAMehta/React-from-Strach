1c86c8772c4c3434d66713d8f4ac1aa20f1f605d
PriyankaAMehta <prayankagm@gmail.com>
2025-12-24 09:21:54 +0100

    Adding components, Notes : app-foundation contains notes for components

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello Priyanka"),
-    React.createElement("h2", {}, "Hello Shreyansh"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello Raj"),
-    React.createElement("h2", {}, "Hello Anita"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello Raj"),
-    React.createElement("h2", {}, "Hello Anita"),
-  ]),
-]);
+const Title = () => <h1>This is Title</h1>;
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1>This is Heading component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
