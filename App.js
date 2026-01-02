4f1ce174bcbc033007c2725890b4e106807d2fad
PriyankaAMehta <prayankagm@gmail.com>
2026-01-02 16:26:37 +0100

    each list in react should have a unique key. Waring : each child in the list should have unique key prop. this waring is been solved

--- App.js
+++ App.js
@@ -804,7 +804,7 @@ const Body = () => {
       <div className="Search">Search</div>
       <div className="res-container">
         {resList.map((restaurant) => (
-          <RestaurantCard resData={restaurant} />
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
