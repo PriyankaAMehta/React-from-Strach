84a7c78ed318eac3dff62bf00cbae9f5ecdfb179
PriyankaAMehta <prayankagm@gmail.com>
2026-02-23 22:45:46 +0100

    removed the CORS issue from local host and tested the swiggy api for displaying the restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,9 +10,24 @@ const Body = () => {
   //1st argument is call back function and 2nd is the dependency array.
   useEffect(() => {
     console.log("useEffect called");
+    fetchData();
   }, []);
 
   console.log("body called");
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    setListOfRestaurants(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
 
   return (
     <div className="body">
