74e552687f320c91ade09d36f0a1238dbb4f4281
PriyankaAMehta <prayankagm@gmail.com>
2026-02-27 07:05:35 +0100

    adding childeren component to AppLayout compoenet so that header and footer are always fixed and importing the Outlet component from react router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router"; //creates routing configuration, configuration means it will define what will happen on a specific route
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router"; //creates routing configuration, configuration means it will define what will happen on a specific route
 import AboutComponent from "./components/About";
 import Contact from "./components/Contact";
 import ErrorComponent from "./components/ErrorComponent";
@@ -13,24 +13,29 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
-
+{
+  /** This is a comment in JSX*/
+}
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      { path: "/", element: <Body /> },
+      {
+        path: "/About",
+        element: <AboutComponent />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+    ],
     errorElement: <ErrorComponent />,
-  },
-  {
-    path: "/About",
-    element: <AboutComponent />,
-  },
-  {
-    path: "/Contact",
-    element: <Contact />,
   },
 ]);
 
