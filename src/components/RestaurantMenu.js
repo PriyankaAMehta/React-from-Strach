84d6cbe09f35063e1deca249ed2fe7a6c28380cd
PriyankaAMehta <prayankagm@gmail.com>
2026-03-01 06:25:35 +0100

    dynamic display for menu from API, change the restaurantID in url of the Restaurant menu card component, it will give other restaurant name , menu and other fields

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -11,23 +11,57 @@ const RestaurantMenu = () => {
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=10582&catalog_qa=undefined&submitAction=ENTER"
+      //use mapi instead od dapi for swiggys url. mapi is the api for mobiles and dapi is api for desktop. mapi are somethimes for Slightly more lenient.
+      "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=1072642&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await data.json();
     console.log(json);
     setResInfo(json.data);
   };
 
-  return resInfo === null ? (
-    <Shimmer />
-  ) : (
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card?.info;
+
+  //const { itemCards } =
+  //resInfo?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+  //?.card?.itemCards[0]?.card.info;
+
+  const { itemCards } =
+    resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card;
+  console.log(itemCards);
+
+  //console.log(itemCards);
+
+  return (
     <div className="menu">
-      <h1>{resInfo?.cards[2]?.card?.card?.info.name}</h1>
+      {/** <h1>{resInfo?.cards[2]?.card?.card?.info.name}</h1>, instead of this we will destructure */}
+      <h1>{name}</h1>
+      <p>
+        {/** .join is JavaScript array method call. .join(separator) takes all elements of an array and combines them into a single string, with the specified separator inserted between each element. */}
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
       <h2>Menu</h2>
       <ul>
-        <li>Biryani</li>
-        <li>Burgers</li>
-        <li>Pizza</li>
+        {/**<li>{itemCards[0].card.info.name}</li>
+        <li>{itemCards[1].card.info.name}</li>
+        <li>{itemCards[2].card.info.name}</li>
+        {1.  what are we iterating upon? --> itemCards 
+        2. so do itemCards.map
+        3. this map takes a callback function so write () => {} inside map()
+        4. the callback function takes item , so for each item , i want card.info.name
+        5. and i want a list item so return the <li> inside the map callback function
+        6. whenever you are using a map, always use a key
+        */}
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - {"₹"}
+            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+          </li>
+        ))}
       </ul>
     </div>
   );
