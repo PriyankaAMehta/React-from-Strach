b3c377c1dd73e5d4dd3bd3a0811f5d073a6425e1
PriyankaAMehta <prayankagm@gmail.com>
2026-03-01 06:58:24 +0100

    dynamic routing of restaurant urls using useParams hook given by react router

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router";
+import { Menu_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
 
   useEffect(() => {
     fetchMenu();
@@ -12,7 +16,7 @@ const RestaurantMenu = () => {
   const fetchMenu = async () => {
     const data = await fetch(
       //use mapi instead od dapi for swiggys url. mapi is the api for mobiles and dapi is api for desktop. mapi are somethimes for Slightly more lenient.
-      "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=1072642&catalog_qa=undefined&submitAction=ENTER"
+      Menu_API + resId
     );
     const json = await data.json();
     console.log(json);
@@ -24,14 +28,15 @@ const RestaurantMenu = () => {
   }
 
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[2]?.card?.card?.info;
+    resInfo?.cards[2]?.card?.card?.info || {};
 
   //const { itemCards } =
   //resInfo?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
   //?.card?.itemCards[0]?.card.info;
 
   const { itemCards } =
-    resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card;
+    resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+      ?.card || {};
   console.log(itemCards);
 
   //console.log(itemCards);
@@ -42,7 +47,7 @@ const RestaurantMenu = () => {
       <h1>{name}</h1>
       <p>
         {/** .join is JavaScript array method call. .join(separator) takes all elements of an array and combines them into a single string, with the specified separator inserted between each element. */}
-        {cuisines.join(", ")} - {costForTwoMessage}
+        {cuisines?.join(", ")} - {costForTwoMessage}
       </p>
       <h2>Menu</h2>
       <ul>
@@ -56,7 +61,7 @@ const RestaurantMenu = () => {
         5. and i want a list item so return the <li> inside the map callback function
         6. whenever you are using a map, always use a key
         */}
-        {itemCards.map((item) => (
+        {itemCards?.map((item) => (
           <li key={item.card.info.id}>
             {item.card.info.name} - {"₹"}
             {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
