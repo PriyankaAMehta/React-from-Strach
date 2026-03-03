a2832a40f8c76a654ec97455f540d16863b9d953
PriyankaAMehta <prayankagm@gmail.com>
2026-03-03 15:18:58 +0100

    functional component vs Class components in React

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 const User = ({ name, location, contact }) => {
+  const [count, setCount] = useState(0);
+  const [count2] = useState(1);
+
   return (
     <div className="user-card">
       <h2>Name : {name}</h2>
       <h3>Location : {location}</h3>
       <h4>Contact : {contact} </h4>
+      <h2>Count = {count}</h2>
+      <h2>Count2 = {count2}</h2>
+      <button
+        onClick={() => {
+          data = count + 1;
+          setCount(data);
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 };
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,15 +3,23 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2: 1,
+    };
   }
 
   render() {
     const { name } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
         <h2>Name : {name}</h2>
         <h3>Location : Hanau</h3>
         <h4>Contact : @mehta_pri </h4>
+        <h2>Count = {count}</h2>
+        <h2>Count2 = {count}</h2>
       </div>
     );
   }
